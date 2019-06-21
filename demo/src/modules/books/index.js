1bb54807ed18b14bfce21d5040d6cb1a792d3d7e
Juan Manuel <sjuanma@gmail.com>
2019-06-21 13:36:43 +0200

    removed some useless comments

--- demo/src/modules/books/index.js
+++ demo/src/modules/books/index.js
@@ -1,9 +1,2 @@
 import { MainRouter } from "./routers/main";
 export default MainRouter;
-
-//No validos por el plugin:
-
-//export { MainRouter } from "./routers/main";
-
-/*import { MainRouter } from "./routers/main";
-export { MainRouter };*/
