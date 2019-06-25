fbe2d87c6f523fe835ddbda14b3707167b959fe1
Juan Manuel <juanmanuel.galvezalcazar@amaris.com>
2019-06-25 11:24:07 +0200

    code changes after revision

--- demo/src/data/authors-and-books/selectors/authorsAndBooks.js
+++ demo/src/data/authors-and-books/selectors/authorsAndBooks.js
@@ -4,13 +4,19 @@ import { authorsCollection, authorsBooksCollection } from "../../authors";
 
 // Example of a Selector with concurrent accesses to different origins and selectors
 export const authorsAndBooks = new Selector(
-  authorsCollection,
-  {
-    source: authorsBooksCollection,
-    query: () => ({ queryString: { all: true } })
-  },
-  booksCollection,
-  (authorsResults, authorsAndBooksResults, booksResults) => {
+  [
+    authorsCollection,
+    {
+      source: authorsBooksCollection,
+      query: () => ({ queryString: { all: true } })
+    },
+    booksCollection
+  ],
+  results => {
+    const authorsResults = results[0],
+      authorsAndBooksResults = results[1],
+      booksResults = results[2];
+
     const result = Object.keys(authorsAndBooksResults).map(authorId => {
       const author = authorsResults.find(author => author.id === authorId);
       const bookIds = authorsAndBooksResults[authorId];
